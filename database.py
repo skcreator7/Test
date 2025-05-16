21a675ae5c53c6320ab6fad50f365745221432f2
skcreator 7 <118694333+skcreator7@users.noreply.github.com>
2025-05-16 15:46:58 +0530

    Update database.py

--- database.py
+++ database.py
@@ -5,10 +5,11 @@ import logging
 logger = logging.getLogger(__name__)
 
 class Database:
-    def __init__(self, uri: str, db_name: str):
+    def __init__(self, uri: str, db_name: str = "movie_bot"):  # Added default db_name
         self.client = AsyncIOMotorClient(uri)
         self.db = self.client[db_name]
         self.posts = self.db.posts
+        logger.info(f"Connected to MongoDB database: {db_name}")
 
     async def initialize(self):
         """Create indexes"""
